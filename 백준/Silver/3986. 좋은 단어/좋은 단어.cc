b8e92c21e62d81548491bd91e2fb2485fc319362
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-06-03 19:46:50 +0900

    [Silver IV] Title: 좋은 단어, Time: 20 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int N, cnt=0;
+    string str;
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        stack<char> s;
+        cin >> str;
+        for(int j=0; j<str.length(); j++) {
+            if(s.empty()) s.push(str[j]);
+            else if(s.top()==str[j]) s.pop();
+            else s.push(str[j]);
+        }
+        if(s.empty()) cnt++;   
+    }
+    cout << cnt;
+}
