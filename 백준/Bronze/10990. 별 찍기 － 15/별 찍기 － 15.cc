ba5ff080b88e62ea30d9dbdbe4a826615ae6c74d
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-06-16 20:14:46 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        for(int j=i+1; j<N; j++) {
+            cout << ' ';
+        }
+        if(i!=0) cout << '*';
+        for(int j=0; j<i*2-1; j++) {
+            cout << ' ';
+        }
+        cout << '*' << '\n';
+    }
+}
