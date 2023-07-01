d13c9dddc0e31db9caa21fa281a2137a32915429
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-07-01 22:13:55 +0900

    [Silver IV] Title: 수학은 체육과목 입니다 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/22351. 수학은 체육과목 입니다 3/수학은 체육과목 입니다 3.cc
+++ 백준/Silver/22351. 수학은 체육과목 입니다 3/수학은 체육과목 입니다 3.cc
@@ -5,32 +5,22 @@ using namespace std;
 int main() {
     string str, str2;
     int a, b;
-    bool bl = false;
     cin >> str;
-    while(1) {
-        if(bl) break;
-        for(int i=1; i<=4; i++) {
-            if(bl) break;
-            for(int j=1; j<=4; j++) {
-                str2="";
-                if(i+j>str.length()) continue;
-                a = stoi(str.substr(0,i));
-                b = stoi(str.substr(str.length()-j));
-                if(a<b && b<=2889) {
-                    for(int i=a; i<=b; i++) {
-                        str2 += to_string(i);
-                    }
-                    if(str==str2) {
-                        cout << a << ' ' << b;
-                        bl = true;
-                        break;
-                    }
+    for(int i=1; i<=4; i++) {
+        for(int j=1; j<=4; j++) {
+            str2="";
+            if(i>str.length()||j>str.length()) continue;
+            a = stoi(str.substr(0,i));
+            b = stoi(str.substr(str.length()-j));
+            if(a<=b && b<=2889) {
+                for(int i=a; i<=b; i++) {
+                    str2 += to_string(i);
+                }
+                if(str==str2) {
+                    cout << a << ' ' << b;
+                    return 0;
                 }
             }
-        }
-        if(!bl) {
-            cout << str << ' ' << str;
-            break;
         }
     }
 }
