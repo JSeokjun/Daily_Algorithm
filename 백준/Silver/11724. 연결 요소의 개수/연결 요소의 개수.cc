54a410174bef0f8c87ae4ebc7a039642a92d42b8
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-07-30 02:36:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 300 ms, Memory: 6400 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> g[1001];
+bool visited[1001] = {false};
+int y;
+
+void dfs(int x) {
+    visited[x] = true;
+
+    for(int i=0; i<g[x].size(); i++) {
+        y = g[x][i];
+        if(!visited[y]) dfs(y);
+    }
+}
+
+int main() {
+    int N, M, u, v, cnt=0;
+    cin >> N >> M;
+
+    for(int i=0; i<M; i++) {
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    for(int i=1; i<=N; i++) {
+        if(!visited[i]) {
+            dfs(i);
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}
