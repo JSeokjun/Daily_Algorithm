000af10bd4fad774ee055937c93161da89b11af8
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-08-14 21:15:41 +0900

    [Bronze III] Title: 전투 드로이드 가격, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.cc
+++ 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.cc
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
     int T, A, B, C, D, E;
-    double cost;
     cin >> T;
     for(int i=0; i<T; i++) {
         cin >> A >> B >> C >> D >> E;
-        cost = A*350.34 + B*230.9 + C*190.55 + D*125.3 + E*180.9;
-        printf("%c%.2lf", '$', cost);
-        cout << '\n';
+        cout << fixed;
+        cout.precision(2);
+        cout << '$' << A*350.34 + B*230.9 + C*190.55 + D*125.3 + E*180.9 << '\n';
     }
 }
