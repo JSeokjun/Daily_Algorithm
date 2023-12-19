78ffc5612fc9a3217b6ada6837807814e92761e9
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2023-12-19 18:14:51 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    int N; cin >> N;
-    int tmp;
-    set<int> A;
+    int N, tmp; cin >> N;
+    vector<int> A;
 
     for(int i=1; i<=N; i++) {
         cin >> tmp;
-        A.insert(tmp);
+        A.push_back(tmp);
     }
+    sort(A.begin(),A.end());
 
     int M; cin >> M;
 
     for(int i=1; i<=M; i++) {
         cin >> tmp;
-        auto it = A.find(tmp);
-        if(it!=A.end()) cout << 1 << '\n';
+        int pos = lower_bound(A.begin(), A.end(), tmp) - A.begin();
+        
+        if(pos==A.size()) cout << 0 << '\n';
+        else if(A[pos]==tmp) cout << 1 << '\n';
         else cout << 0 << '\n';
     }
 
