684b864442bf4120ea4848ab3a02b9f2207b12ff
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-01-11 18:22:31 +0900

    [Silver I] Title: 목차 세기, Time: 128 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Silver/25956. 목차 세기/목차 세기.cc
+++ 백준/Silver/25956. 목차 세기/목차 세기.cc
@@ -1,37 +1,45 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int level[1000000], num[1000000], n;
-    
-    cin >> n >> level[0];
+    int N; cin >> N;
+    stack<int> s1, s2;
+    int tmp; cin >> tmp;
+    int* cnt = new int[1000000];
 
-    if (level[0] != 1) {
+    if(tmp != 1) {
         cout << -1;
         return 0;
     }
+    else s1.push(tmp);
     
-    for (int i = 1; i < n; i++) {
-        cin >> level[i];
-
-        if (level[i - 1] + 1 < level[i]) {
+    for(int i=0; i<N-1; i++) {
+        int L; cin >> L;
+        if(s1.top()+1 < L) {
             cout << -1;
             return 0;
         }
+        s1.push(L);
+        cnt[L] = 0;
     }
 
-    for (int i = 0; i < n; i++) {
-        int cnt = 0;
-        for (int j = i + 1; j < n; j++) {
-            if (level[j] == level[i] + 1) cnt++;
-            if (level[j] == level[i]) break;
-        }
-        num[i] = cnt;
-    }
+    while(!s1.empty()) {
+		s2.push(cnt[s1.top()]);
+		cnt[s1.top()] = 0;
+		cnt[s1.top() - 1]++;
+		s1.pop();
+	}
 
-    for (int i = 0; i < n; i++) cout << num[i] << '\n';
-    
+	while(!s2.empty()) {
+		cout << s2.top() << '\n';
+		s2.pop();
+	}
+
+    delete[] cnt;
+
     return 0;
 }
