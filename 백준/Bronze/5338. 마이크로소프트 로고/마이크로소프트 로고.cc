ab0b9e1765f518f0bb93f159e9a46df9113e3d88
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-01-23 20:14:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    cout << "       _.-;;-._" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-;;-._|" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-''-._|" << '\n';
+
+    return 0;
+}   
