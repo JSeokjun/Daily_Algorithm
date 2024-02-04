16dbd54808b310b116c7c8cd063d44b91f70ce22
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-02-04 17:45:29 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int N, M; cin >> N >> M;
+    int L[101]{0}, S[101]{0};
+
+    for(int i=0; i<N; i++) {
+        int x, y; cin >> x >> y;
+        L[x] = y;
+    }
+    for(int i=0; i<M; i++) {
+        int u, v; cin >> u >> v;
+        S[u] = v;
+    }
+
+    queue<pair<int,int>> Q;
+    bool visit[101]{0};
+    Q.push({1,0});
+    visit[1] = 1;
+
+    while(1) {
+        int num = Q.front().first;
+        int cnt = Q.front().second;
+        
+        Q.pop();
+
+        for(int i=1; i<=6; i++) {
+            if(!visit[num+i] && num+i < 100) {
+                if(L[num+i] != 0) Q.push({L[num+i],cnt+1});
+                else if(S[num+i] != 0) Q.push({S[num+i],cnt+1});
+                else Q.push({num+i,cnt+1});
+                visit[num+i] = 1;
+            }
+            else if(num+i == 100) {
+                cout << cnt+1;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}   
