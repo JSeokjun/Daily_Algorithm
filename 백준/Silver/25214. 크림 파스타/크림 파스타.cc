a301d1c9ee38476931dafada115f67dec19b5553
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-02-08 22:06:34 +0900

    [Silver IV] Title: 크림 파스타, Time: 52 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int N, min, max=0; cin >> N >> min; N--;
+    vector<int> V;
+    V.push_back(0);
+
+    while(N--) {
+        int a; cin >> a;
+
+        if(a-min > max) {
+            max = a-min;
+            V.push_back(max);
+        }
+        else V.push_back(max);
+
+        if(a < min) min = a;
+    }
+
+    for(int i=0; i<V.size(); i++) {
+        cout << V[i] << ' ';
+    }
+
+    return 0;
+}   
