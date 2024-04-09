fd3d28e62e5e0cf27d86e28a855fd8f86c30680b
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-04-09 18:09:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,22 +1,21 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
     int N, K; cin >> N >> K;
-    int dp[N+1][K+1];
-    for(int i=0; i<=K; i++) dp[0][i] = 0;
+    int dp[K+1]{0};
 
     for(int i=1; i<=N; i++) {
         int w, v; cin >> w >> v;
-        for(int j=0; j<=K; j++) {
-            if(w>j) dp[i][j] = dp[i-1][j];
-            else dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v);
+        for(int j=K; j>=w; j--) {
+            dp[j] = max(dp[j], dp[j-w]+v);
         }
     }
 
-    cout << dp[N][K];
+    cout << *max_element(dp, dp+(K+1));
 
     return 0;
 }
