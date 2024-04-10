1e1feb74f747e5b3bc67f8f49fe658b45a0ff150
SeokjunJ <55570350+JSeokjun@users.noreply.github.com>
2024-04-10 21:25:09 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -10,12 +8,8 @@ int main() {
     int dp[K+1]{0};
     for(int i=1; i<=K; i++) dp[i] = K+1;
 
-    vector<int> V(N+1);
-    for(int i=1; i<=N; i++) cin >> V[i];
-    sort(V.begin(), V.end());
-
     for(int i=1; i<=N; i++) {
-        int v = V[i];
+        int v; cin >> v;
         for(int j=v; j<=K; j++) {
             dp[j] = min(dp[j], dp[j-v]+1);
         }
