c57efacecf3fda99460839ae7d67a369c6b143ae
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-03-11 01:04:30 +0900

    [Silver V] Title: 집합, Time: 620 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,4 +1,52 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//   ios::sync_with_stdio(0);
+//   cin.tie(0);
+
+//   int M; cin >> M;
+//   bool S[20]; for (int i=0; i<20; i++) S[i] = false;
+//   string act = "";
+//   int num = 0;
+
+//   for (int i=0; i<M; i++) {
+//     cin >> act;
+//     if (act != "all" && act != "empty") {
+//       cin >> num;
+//     }
+
+//     if (act == "add") {
+//       S[num-1] = true;
+//     }
+//     else if (act == "remove") {
+//       S[num-1] = false;
+//     }
+//     else if (act == "check") {
+//       cout << S[num-1] << '\n';
+//     }
+//     else if (act == "toggle") {
+//       S[num-1] ? S[num-1] = false : S[num-1] = true;
+//     }
+//     else if (act == "all") {
+//       for (int j=0; j<20; j++) S[j] = true;
+//     }
+//     else if (act == "empty") {
+//       for (int j=0; j<20; j++) S[j] = false;
+//     }
+//     else {
+//       return -1;
+//     }
+//   }
+
+//   return 0;
+// }
+
+/* 비트 마스킹 활용 버전 */
+/* 이 문제는 비트 마스킹을 사용하면 더 효율적으로 구현할 수 있습니다. 20개의 요소를 가진 집합은 하나의 정수(int)로 표현 가능합니다. */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,10 +55,9 @@ int main() {
   cin.tie(0);
 
   int M; cin >> M;
-  bool S[20], all_s[20], empty_s[20];
-  for (int i=0; i<20; i++) S[i] = false;
-  string act = "";
-  int num = 0;
+  int S = 0; // 비트마스크로 집합 표현
+  string act;
+  int num;
 
   for (int i=0; i<M; i++) {
     cin >> act;
@@ -19,25 +66,22 @@ int main() {
     }
 
     if (act == "add") {
-      S[num-1] = true;
+      S |= (1 << (num-1)); // 비트 켜기
     }
     else if (act == "remove") {
-      S[num-1] = false;
+      S &= ~(1 << (num-1)); // 비트 끄기
     }
     else if (act == "check") {
-      cout << S[num-1] << '\n';
+      cout << ((S & (1 << (num-1))) ? 1 : 0) << '\n';
     }
     else if (act == "toggle") {
-      S[num-1] ? S[num-1] = false : S[num-1] = true;
+      S ^= (1 << (num-1)); // XOR로 토글
     }
     else if (act == "all") {
-      for (int j=0; j<20; j++) S[j] = true;
+      S = (1 << 20) - 1; // 모든 비트 켜기
     }
     else if (act == "empty") {
-      for (int j=0; j<20; j++) S[j] = false;
-    }
-    else {
-      return -1;
+      S = 0; // 모든 비트 끄기
     }
   }
 
