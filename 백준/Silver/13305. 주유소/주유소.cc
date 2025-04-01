85b0229dce61d2f6f2ed3a32fc4cb5808274f222
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-04-01 17:35:55 +0900

    [Silver III] Title: 주유소, Time: 52 ms, Memory: 3568 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N; cin >> N;
+  vector<pair<int,int>> V;
+
+  for (int i=0; i<N-1; i++) {
+    int distance; cin >> distance;
+    V.push_back({0,distance});
+  }
+  V.push_back({0,0});
+
+  for (int i=0; i<N; i++) {
+    int price; cin >> price;
+    V[i].first = price;
+  }
+
+  int index = 0, cost = 0;
+
+  while(1) {
+    bool is_break = false; 
+
+    for (int j=index; j<N-1; j++) {
+      if (V[index].first <= V[j].first) {
+        cost += V[index].first * V[j].second;
+      }
+      else {
+        index = j;
+        is_break = true;
+        break;
+      }
+    }
+    if (!is_break) break;
+  }
+
+  cout << cost;
+
+  return 0;
+}
