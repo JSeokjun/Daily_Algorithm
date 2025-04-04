6b10f984c87e80834050d4add71d5317da8218fa
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-04-04 23:23:53 +0900

    [Silver III] Title: 블로그, Time: 20 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cc
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -24,17 +24,38 @@ int main() {
     return 0;
   }
 
-  for (int i=0; i<N; i++) {
+  // for (int i=0; i<N; i++) {
+  //   cnt_visitor += V[i];
+
+  //   if (i == X-1) {
+  //     max_visitor = cnt_visitor;
+  //   }
+  //   else if (i > X-1) {
+  //     cnt_visitor -= V[i-X];
+  //   }
+  //   else continue;
+
+  //   if (cnt_visitor > max_visitor) {
+  //     max_visitor = cnt_visitor;
+  //     num_range = 1;
+  //   }
+  //   else if (cnt_visitor == max_visitor) {
+  //     num_range++;
+  //   }
+  // }
+
+  // 첫 X일 동안의 방문자 수 계산
+  for (int i=0; i<X; i++) {
     cnt_visitor += V[i];
+  }
+  
+  max_visitor = cnt_visitor;
+  num_range = 1;
 
-    if (i == X-1) {
-      max_visitor = cnt_visitor;
-    }
-    else if (i > X-1) {
-      cnt_visitor -= V[i-X];
-    }
-    else continue;
-
+  // 슬라이딩 윈도우로 나머지 기간 확인
+  for (int i=X; i<N; i++) {
+    cnt_visitor += V[i] - V[i-X];  // 새 날짜 추가, 오래된 날짜 제거
+    
     if (cnt_visitor > max_visitor) {
       max_visitor = cnt_visitor;
       num_range = 1;
@@ -48,3 +69,14 @@ int main() {
  
   return 0;
 }
+
+// 개선 사항
+
+// 1. 슬라이딩 윈도우 알고리즘을 더 명확하게 구현:
+// 먼저 첫 X일 동안의 방문자 수를 계산
+// 이를 초기 최대값으로 설정
+// 이후 윈도우를 한 칸씩 이동하며 최대값 갱신
+
+// 2. 코드 가독성 향상:
+// 불필요한 조건문 제거
+// 로직을 더 명확하게 분리
