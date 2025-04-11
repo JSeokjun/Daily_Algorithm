d607dfa6b85b5f0592b849039b8d52fc7e3b0e3b
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-04-11 18:24:38 +0900

    Update 타노스.cc

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -1,5 +1,8 @@
+// 큐와 벡터를 활용한 버전
+
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -8,16 +11,17 @@ int main() {
 	cin.tie(0);
 
     string S; cin >> S;
-    vector<int> V;
+    queue<int> Q;
+    vector<int> V, answer;
     int num_zero = 0, num_one = 0;
 
     for (int i=0; i<S.size(); i++) {
         if (S[i] == '0') {
-            V.push_back(0);
+            Q.push(0);
             num_zero++;
         }
         else {
-            V.push_back(1);
+            Q.push(1);
             num_one++;
         }
     }
@@ -25,30 +29,30 @@ int main() {
     num_zero /= 2;
     num_one /= 2;
 
-    int i = V.size()-1;
-
-    while(num_zero) {
-        if (V[i] == 0) {
-            V.erase(V.begin()+i);
-            num_zero--;
-
-            if (i >= V.size()) i--;
-        }
-        else i--;
-    }
-
-    i = 0;
-
-    while (num_one) {
-        if (V[i] == 1) {
-            V.erase(V.begin()+i);
+    while(!Q.empty()) {
+        if (num_one != 0 && Q.front() != 0) {
+            Q.pop();
             num_one--;
         }
-        else i++;
+        else {
+            V.push_back(Q.front());
+            Q.pop();
+        }
     }
 
-    for (int i=0; i<V.size(); i++) {
-        cout << V[i];
+    while (!V.empty()) {
+        if (num_zero != 0 && V.back() == 0) {
+            V.pop_back();
+            num_zero--;
+        }
+        else {
+            answer.push_back(V.back());
+            V.pop_back();
+        }
+    }
+
+    for (int i=answer.size()-1; i>=0; i--) {
+        cout << answer[i];
     }
 
     return 0;
