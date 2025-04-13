5baa9bd16a6222ae1266ec29531600bf56476cae
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-04-13 18:58:54 +0900

    [Silver III] Title: 가희와 키워드, Time: 1292 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.cc
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
-	cin.tie(0);
+    cin.tie(0);
 
     int N, M; cin >> N >> M;
     set<string> note;
@@ -19,17 +19,15 @@ int main() {
         string keyword_list; cin >> keyword_list;
         string keyword = "";
 
-        for (int i=0; i<keyword_list.size(); i++) {
-            if (keyword_list[i] == ',') {
-                note.erase(keyword);
+        for (int i=0; i<=keyword_list.size(); i++) {
+            if (i == keyword_list.size() || keyword_list[i] == ',') {
+                // 메모장에 있는 키워드만 삭제
+                if (!keyword.empty() && note.find(keyword) != note.end()) {
+                    note.erase(keyword);
+                }
                 keyword = "";
-                continue;
-            }
-            
-            keyword += keyword_list[i];
-
-            if (i == keyword_list.size()-1) {
-                note.erase(keyword);
+            } else {
+                keyword += keyword_list[i];
             }
         }
 
@@ -38,3 +36,8 @@ int main() {
 
     return 0;
 }
+
+// 개선 내용:
+// 이제 메모장에 있는 키워드만 삭제합니다 (note.find(keyword) != note.end() 조건 추가)
+// 문자열 끝과 쉼표를 같은 로직으로 처리하여 코드 중복을 제거했습니다.
+// 빈 키워드(empty())를 체크하여 오류를 방지했습니다.
