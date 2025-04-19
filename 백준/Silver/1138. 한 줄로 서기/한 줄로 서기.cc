81ce246d787158811420b952010c94c98fd2d891
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-04-19 17:02:41 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
 
 int main() {
@@ -10,47 +9,37 @@ int main() {
     int N; cin >> N;
     vector<pair<int,int>> V;
     vector<int> answer(N, 0);
-    set<int> S;
  
     for (int i=1; i<=N; i++) {
         int num; cin >> num;
         V.push_back({i, num});
-        S.insert(i);
     }
 
     answer[V[0].second] = 1;
-    S.erase(1);
-    
-    while (!S.empty()) {
 
-        for (int i=0; i<N; i++) {
-            if (S.find(i+1) == S.end()) continue;
+    for (int i=1; i<N; i++) {
+        int possible_cnt = 0;
+        int possible_index = 0;
+        int height = V[i].first;
+        int taller = V[i].second;
 
-            int possible_cnt = 0;
-            int possible_index = 0;
-            int height = V[i].first;
-            int taller = V[i].second;
+        for (int j=taller; j<N; j++) {
+            if (answer[j] != 0) continue; 
+            int cnt = 0;
 
-            for (int j=taller; j<N; j++) {
-                if (answer[j] != 0) continue; 
-                int cnt = 0;
-
-                for (int k=0; k<j; k++) {
-                    if (answer[k] == 0 || answer[k] > height) cnt++;
-                }
-
-                if (cnt == taller) {
-                    possible_cnt++;
-                    possible_index = j;
-                }
+            for (int k=0; k<j; k++) {
+                if (answer[k] == 0 || answer[k] > height) cnt++;
             }
 
-            if (possible_cnt == 1) {
-                answer[possible_index] = height;
-                S.erase(height);
+            if (cnt == taller) {
+                possible_cnt++;
+                possible_index = j;
             }
         }
-        break;
+
+        if (possible_cnt == 1) {
+            answer[possible_index] = height;
+        }
     }
 
     for (int i=0; i<N; i++) {
