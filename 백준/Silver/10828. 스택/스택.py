d849e40e31c480c9b437752f0bce1aa3f65ec929
장석준 <55570350+JSeokjun@users.noreply.github.com>
2025-05-16 17:23:01 +0900

    [Silver IV] Title: 스택, Time: 412 ms, Memory: 112064 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+stack = []
+
+for _ in range(N):
+    command = list(map(str, input().split()))
+
+    if command[0] == 'push':
+        stack.append(command[1])
+
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            x = stack.pop()
+            print(x)
+
+    elif command[0] == 'size':
+        print(len(stack))
+
+    elif command[0] == 'empty':
+        if len(stack):
+            print(0)
+        else:
+            print(1)
+
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+    else:
+        print(-1)
